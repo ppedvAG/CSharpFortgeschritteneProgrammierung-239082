ad2aa949e0f65cf8aaf01ad12acbdc65698b4457
ppedvLukasK <lukask@ppedv.de>
2024-04-08 12:31:10 +0200

    M001-M002

---
+++ Sonstiges/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+
+namespace Sonstiges;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Wagon w1 = new() { AnzSitze = 30, Farbe = "Rot" };
+		Wagon w2 = new() { AnzSitze = 30, Farbe = "Rot" };
+        Console.WriteLine(w1 == w2);
+
+		Zug z = new();
+		z += w1;
+		z = z + w2;
+
+		Zug z2 = new();
+		z2 += new Wagon();
+
+		z += z2;
+
+		Zug z3 = w1;
+
+		foreach (Wagon w in z) //statt z.Wagons
+		{
+
+		}
+
+		Console.WriteLine(z[2]);
+        Console.WriteLine(z[30, "Rot"]);
+    }
+}
+
+public class Zug : IEnumerable<Wagon>
+{
+	private List<Wagon> Wagons = [];
+
+	public IEnumerator<Wagon> GetEnumerator()
+	{
+		foreach (Wagon w in Wagons)
+			yield return w;
+	}
+
+	IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+
+	public Wagon this[int index]
+	{
+		get => Wagons[index];
+		set
+		{
+			if (index > Wagons.Count)
+				Wagons.Add(value);
+			else
+				Wagons[index] = value;
+		}
+	}
+
+	public Wagon this[int anzSitze, string farbe]
+	{
+		get => Wagons.FirstOrDefault(e => e.AnzSitze == anzSitze && e.Farbe == farbe);
+	}
+
+	public static Zug operator +(Zug z, Wagon w)
+	{
+		z.Wagons.Add(w);
+		return z;
+	}
+
+	public static Zug operator +(Zug z1, Zug z2)
+	{
+		z1.Wagons.AddRange(z2.Wagons);
+		return z1;
+	}
+
+	/// <summary>
+	/// Wagon zu Zug konvertieren
+	/// </summary>
+	public static implicit operator Zug(Wagon w)
+	{
+		return new Zug(); //w einbauen
+	}
+}
+
+public class Wagon
+{
+	public int AnzSitze { get; set; }
+
+	public string Farbe { get; set; }
+
+	public static bool operator ==(Wagon a, Wagon b) => a.AnzSitze == b.AnzSitze && a.Farbe == b.Farbe;
+
+	public static bool operator !=(Wagon a, Wagon b) => !(a == b);
+}
---
+++ Sprachfeatures/Program.cs
@@ -0,0 +1,169 @@
+﻿using System.Numerics;
+using Point4D = (int, int, int, int);
+using Group = System.Collections.Generic.IEnumerable<System.Linq.IGrouping<int, int>>;
+
+namespace Sprachfeatures;
+
+internal class Program
+{
+	int referenztyp = 10;
+
+	static void Main(string[] args)
+	{
+		decimal d = 325_325_823_857.328_521_348_243m;
+        Console.WriteLine(d);
+
+        Console.WriteLine(2.3E9); //2.3 * 10^9
+
+		//class und struct
+
+		//class
+		//Referenztyp
+		//Wenn zwei Variablen von Referenztypen verglichen werden, werden die Speicheradressen (Hash Codes) verglichen
+		Program p = new Program(); //Zeiger auf das unterliegende Objekt
+		Program p2 = p; //Zeiger auf das Objekt unter p
+		p.referenztyp = 100; //Der Wert wird hinter beiden Variablen verändert
+
+		//struct
+		//Wertetyp
+		//Wenn zwei Variablen von Wertetypen verglichen werden, werden die Inhalte verglichen
+		int x = 10; //Wert anlegen und in x speichern
+		int y = x; //Den Wert hinter x in y kopieren
+		x = 100; //y bleibt unberührt
+
+		ref int z = ref x; //Hier wird ein Zeiger zu x hergestellt
+		x = 200; //z wird auch verändert
+
+		void Test(int x) { } //Ohne ref wird hier eine Kopie erzeugt, originaler Wert wird nicht verändert
+
+		void Test2(ref int x) => Console.WriteLine(x);
+
+		//Null-Coalescing Operator (??-Operator): Nimm die linke Seite, wenn diese nicht null ist, sonst die rechte Seite
+		string str = null;
+		int a;
+		if (str == null)
+			a = 0;
+		else
+			a = str.Length;
+
+		a = str == null ? 0 : str.Length;
+
+		a = str?.Length ?? 0;
+
+		//Switch Pattern
+		int b = a switch
+		{
+			0 => 1,
+			1 => 5,
+			2 => 13
+		};
+
+		string Test3() => DateTime.Now.DayOfWeek switch
+		{
+			DayOfWeek.Sunday => throw new NotImplementedException(),
+			DayOfWeek.Monday => throw new NotImplementedException(),
+			DayOfWeek.Tuesday => throw new NotImplementedException(),
+			DayOfWeek.Wednesday => throw new NotImplementedException(),
+			DayOfWeek.Thursday => throw new NotImplementedException(),
+			DayOfWeek.Friday => throw new NotImplementedException(),
+			DayOfWeek.Saturday => throw new NotImplementedException(),
+			_ => throw new NotImplementedException()
+		};
+
+		static void Test4()
+		{
+            //Console.WriteLine(a);
+        }
+
+		//String-Interpolation ($-String): Ermöglicht, Code in einen String einzubauen
+		string kombination = "Die Zahl ist: " + a + ", die Kommazahl ist: " + d + ", der String ist: " + str;
+		string interpolation = $"Die Zahl ist: {{{a}}}, die Kommazahl ist: {d}, der String ist: {str}";
+
+		Console.WriteLine(kombination);
+        Console.WriteLine(interpolation);
+
+		//Verbatim-String (@-String): Ignoriert Escape-Sequenzen
+		string escape = @"\n\t\\";
+		string pfad = @"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\8.0.1\System.Security.AccessControl.dll";
+
+		string tag = DateTime.Now.DayOfWeek switch
+		{
+			//&&, || -> and, or
+			>= DayOfWeek.Monday and <= DayOfWeek.Friday => "Unter der Woche",
+			DayOfWeek.Saturday or DayOfWeek.Sunday => "Wochenende"
+		};
+
+		if (DateTime.Now.DayOfWeek == DayOfWeek.Monday || DateTime.Now.DayOfWeek == DayOfWeek.Thursday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+		{
+
+		}
+
+		if (DateTime.Now.DayOfWeek is DayOfWeek.Monday or DayOfWeek.Thursday or DayOfWeek.Sunday)
+		{
+
+		}
+
+		char c = 'a';
+		if (c is >= 'a' and <= 'z')
+		{
+
+		}
+
+		Person person = new Person(1, "Max");
+		//person.ID = 5;
+
+		BigInteger big = 75983275982375983;
+		big = big * big;
+
+		List<int> ints = new();
+		ints.Add(1);
+		ints.Add(2);
+		ints.Add(3);
+
+		ints = new() { 1, 2, 3 };
+
+		ints = new() { Capacity = 10 };
+
+		Fahrzeug fzg = new();
+		fzg.Name = "Hallo";
+		fzg.Version = "1.0";
+
+		// ->
+
+		Fahrzeug fzg2 = new() { Name = "Hallo", Version = "1.0" };
+
+		//void CheckFahrzeug(Fahrzeug f!!)
+		//{
+		//	if (f is null)
+		//	{
+		//		throw new ArgumentNullException(nameof(f));
+		//	}
+		//}
+
+		//https://github.com/dotnet/runtime
+
+		Point4D p4d = (1, 2, 3, 4);
+		Group g = ints.GroupBy(e => e);
+	}
+
+	public class Fahrzeug(string name, string description, string author, string version)
+	{
+		public string Name { get; set; } = name;
+
+		public string Description { get; set; } = description;
+
+		public string Author { get; set; } = author;
+
+		public string Version { get; set; } = version;
+
+		public void Test()
+		{
+            Console.WriteLine(name);
+        }
+	}
+
+	public record Person(int ID, string Name)
+	{
+		public void Test() { }
+	}
+}
