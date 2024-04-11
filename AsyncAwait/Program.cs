64ebdeafdde7e27964c1a36cb7dc7c5fe67c459e
ppedvLukasK <lukask@ppedv.de>
2024-04-11 11:47:10 +0200

    M007

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace AsyncAwait;
+
+internal class Program
+{
+	static async Task Main(string[] args)
+	{
+		//async/await
+		//Effektiv wie TPL, aber simpler
+
+		//Drei Teile:
+		//Aufgabe starten
+		//Zwischenschritte
+		//Auf Aufgabe warten
+
+		//Ohne Async/Await
+		//Task<string> t = Task.Run(() => File.ReadAllText("Pfad.txt"));
+		//string output = t.Result;
+
+		////Mit Async/Await
+		//Task<string> t2 = File.ReadAllTextAsync("Pfad.txt");
+		//string output2 = await t2; //Bei await kommt das Ergebnis heraus (effektiv t2.Result)
+
+		////->
+		//string output3 = await File.ReadAllTextAsync("Pfad.txt");
+
+		////Vorteil: Blockiert nicht die GUI
+
+		////Ohne async/await
+		//List<Task> xmlTasks = [];
+		//for (int i = 0; i < 10; i++)
+		//{
+		//	xmlTasks.Add(Task.Run(() => Thread.Sleep((Random.Shared.Next() % 2000) + 1000))); //10 Tasks die jeweils zw. 1s und 3s warten
+		//}
+		//Task.WaitAll([.. xmlTasks]);
+
+		////Mit async/await
+		////WICHTIG: Methode selbst muss async sein
+		//List<Task> xmlTasks2 = [];
+		//for (int i = 0; i < 10; i++)
+		//{
+		//	xmlTasks.Add(Task.Delay((Random.Shared.Next() % 2000) + 1000)); //10 Tasks die jeweils zw. 1s und 3s warten
+		//	//Task.Delay: Effektiv Thread.Sleep aber awaitable
+		//}
+		//await Task.WhenAll(xmlTasks2); //Selbiges wie Task.WaitAll, aber blockiert nicht die GUI
+
+		//Wird als Task gestartet
+		Test();
+
+		//Asynchroner Methodenaufruf
+		Console.WriteLine("Test2 gestartet"); //1s
+		await Test2();
+		Console.WriteLine("Test2 fertig"); //1s
+
+		Task<string> t = Test3();
+		string ergebnis = await t;
+        await Console.Out.WriteLineAsync(ergebnis);
+
+        Console.ReadKey();
+	}
+
+	//Drei verschiedene von Async-Methoden
+	//async void: Diese Methode kann await benutzen, aber selbst nicht awaited werden
+	//async Task: Diese Methode kann await benutzen und kann awaited werden
+	//async Task<T>: Diese Methode kann await benutzen, kann awaited werden, und hat ein Ergebnis
+
+	public static async void Test()
+	{
+		Console.WriteLine("Test gestartet");
+		await Task.Delay(3000);
+		Console.WriteLine("Test fertig");
+	}
+
+	public static async Task Test2()
+	{
+		await Task.Delay(3000);
+	}
+
+	public static async Task<string> Test3()
+	{
+		await Task.Delay(3000);
+		return "Hallo";
+	}
+}
---
+++ AsyncAwaitWPF/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwaitWPF;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		List<Task> xmlTasks = [];
+		for (int i = 0; i < 10; i++)
+		{
+			xmlTasks.Add(Task.Run(() => Thread.Sleep((Random.Shared.Next() % 2000) + 1000))); //10 Tasks die jeweils zw. 1s und 3s warten
+		}
+		Task.WaitAll([.. xmlTasks]); //Blockierende Operation
+		Info.Text += "Tasks fertig";
+	}
+
+	private void Button_Click_ContinueWith(object sender, RoutedEventArgs e)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Task t = new Task(() => Thread.Sleep((Random.Shared.Next() % 2000) + 1000));
+			//t.ContinueWith(x => Info.Text += "Task fertig\n"); //Nicht möglich, da Zugriffe von Hintergrundsthreads auf den Hauptthread nicht erlaubt sind
+			t.ContinueWith(x => Dispatcher.Invoke(() => Info.Text += "Task fertig\n")); //Dispatcher.Invoke: Führt Code auf dem Main Thread aus
+			t.Start();
+		}
+	}
+
+	private async void Button_Click_Async(object sender, RoutedEventArgs e)
+	{
+		List<Task> xmlTasks = [];
+		for (int i = 0; i < 10; i++)
+		{
+			xmlTasks.Add(Task.Delay((Random.Shared.Next() % 2000) + 1000)); //10 Tasks die jeweils zw. 1s und 3s warten
+		}
+		await Task.WhenAll([.. xmlTasks]); //Blockiert nicht
+		Info.Text += "Tasks fertig";
+	}
+
+	private void Button_Click_ContinueWith_Async(object sender, RoutedEventArgs e)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Task.Delay((Random.Shared.Next() % 2000) + 1000).ContinueWith(x => Dispatcher.Invoke(() => Info.Text += "Task fertig\n"));
+		}
+	}
+
+	private async void Request(object sender, RoutedEventArgs e)
+	{
+		//Ohne async/await
+		using HttpClient client = new();
+		//Task t = Task.Run(() => client.GetAsync("http://www.gutenberg.org/files/54700/54700-0.txt"))
+		//			 .ContinueWith(x => Dispatcher.Invoke(() =>
+		//			 {
+		//				 Task<string> c = Task.Run(() => x.Result.Content.ReadAsStringAsync());
+		//				 Info.Text = "Text wird ausgelesen...";
+		//				 Info.Text = c.Result;
+		//			 }));
+		//Info.Text = "Text wird geladen...";
+
+		//Mit async/await
+		try
+		{
+			Task<HttpResponseMessage> t = client.GetAsync("http://www.gutenberg.org/files/54700/54700-0.txt"); //Aufgabe starten
+			Info.Text = "Text wird geladen..."; //Zwischenschritte
+			HttpResponseMessage message = await t; //Auf Aufgabe warten
+
+			Task<string> content = message.Content.ReadAsStringAsync();
+			Info.Text = "Text wird ausgelesen...";
+			string text = await content;
+
+			Info.Text = text;
+		}
+		catch (InvalidOperationException ex)
+		{
+			Info.Text = ex.Message;
+        }
+	}
+}
